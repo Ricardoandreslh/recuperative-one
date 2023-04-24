bc74273678fcd5dd912752de9501d072d7574893
AngelEAL <arraizangel5@gmail.com>
2023-04-24 12:01:01 -0400

    Menu Hamburguesa y pie de pagina

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import './style.css';
+
+//Acción OPEN & CLOSE de MENU
+
+const toggleButton = document.getElementById('button-menu')
+const navWrapper = document.getElementById('nav')
+
+toggleButton.addEventListener('click',() => {
+  toggleButton.classList.toggle('close')
+  navWrapper.classList.toggle('show')
+})
+
+navWrapper.addEventListener('click',e => {
+  if(e.target.id === 'nav'){
+    navWrapper.classList.remove('show')
+    toggleButton.classList.remove('close')
+  }
+})
